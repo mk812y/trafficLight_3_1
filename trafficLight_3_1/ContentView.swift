c908ceadec0b637e33b2a8665b81656fe9bd12f2
mk812y <mk812y@gmail.com>
2022-02-15 09:44:00 +0300

    Initial Commit

---
+++ trafficLight_3_1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  trafficLight_3_1
+//
+//  Created by Михаил Куприянов on 15.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ trafficLight_3_1/trafficLight_3_1App.swift
@@ -0,0 +1,17 @@
+//
+//  trafficLight_3_1App.swift
+//  trafficLight_3_1
+//
+//  Created by Михаил Куприянов on 15.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct trafficLight_3_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
